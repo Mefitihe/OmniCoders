e7d2917a68a677f0b0b248ebb6e6dbd8ee0e1b98
dejegithub <dtechane6@gmail.com>
2019-09-03 12:19:27 +0300

    Update Option menu

--- app/src/main/java/com/omnicoders/omnicoders/HomeActivity.java
+++ app/src/main/java/com/omnicoders/omnicoders/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.omnicoders.omnicoders;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,35 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        /*use AppCompatActivity method getMenuInflater() to get a handle on the menu inflater*/
+
+        MenuInflater inflater = getMenuInflater();
+        /* use the inflater's inflate method to inflate our home_option layout to this menu */
+        inflater.inflate(R.menu.home_option, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.lang) {
+            Toast.makeText(getApplicationContext(), "Language Selected", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.changePass) {
+            Toast.makeText(getApplicationContext(), "ChangePass Selected", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.theme) {
+            Toast.makeText(getApplicationContext(), "Theme Selected", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.logout) {
+            Intent i = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(i);
+            System.exit(0);
+        }
+        return true;
     }
 }
--- app/src/main/java/com/omnicoders/omnicoders/LoginActivity.java
+++ app/src/main/java/com/omnicoders/omnicoders/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.omnicoders.omnicoders;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,6 +22,10 @@ public class LoginActivity extends AppCompatActivity {
     EditText edtUsername, edtPassword;
     String mUsername, mPassword;
     Context mContext;
+    // Notification ID to allow for future updates
+    private static final int MY_NOTIFICATION_ID = 1;
+    final String CHANNEL_ID = "CHANNEL_ID";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
         edtUsername = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
 
+        createNotificationChannel();
+        //calling login() inside onCreate()
         login();
 
     }
@@ -43,6 +55,10 @@ public class LoginActivity extends AppCompatActivity {
 
                     Intent i = new Intent(LoginActivity.this, HomeActivity.class);
                     startActivity(i);
+
+                    //calling statusNotification() inside login button
+                    statusNotification();
+
                 }
                 else{
                     Toast.makeText(mContext, "Invalid username or Password", Toast.LENGTH_SHORT).show();
@@ -50,7 +66,52 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
+    private void statusNotification() {
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent, 0);
+
+        //Create notification builder
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications)
+                .setContentTitle("Login notification")
+                .setContentText("Successfully Login!")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Hey, you are successfully login into OmniCoders App.\nTab to go home"))
+
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(MY_NOTIFICATION_ID, builder.build());
+
+
+    }
+
+    //create notificationChannel and set its importance
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.log_in);
+            String description = getString(R.string.logo);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
 }
